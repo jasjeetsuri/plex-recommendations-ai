ff466b38fc9b5cc03a744a9259060911d7283ed5
Jas Suri <33685132+jasjeetsuri@users.noreply.github.com>
2025-06-24 19:30:28 +0100

    Update classes.py

--- utils/classes.py
+++ utils/classes.py
@@ -5,9 +5,11 @@ class UserInputs:
     plex_url: str
     plex_token: str
     openai_key: str
-    library_name: str
+    library_names: str
     collection_title: str
     history_amount: int
     recommended_amount: int
     minimum_amount: int
     wait_seconds: int
+    add_to_watchlist: bool
+    create_collections: bool
